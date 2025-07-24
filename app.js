985a0752f02bc2cf0e25f95ffe0f468d366e6cf5
Umer <omiomeromi11@gmail.com>
2025-07-24 09:30:37 +0300

    Create a global error handling middleware and add it in app.js

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())//reads cookies from incoming request
+
+
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription tracker API!");
