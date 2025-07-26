be62a9eee6b151cf248182ba2d22232aae16d679
Umer <omiomeromi11@gmail.com>
2025-07-26 18:22:56 +0300

    Finalize and clean code

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -5,21 +5,24 @@ import { createSubscription, getAllSubscriptions, getSubscriptionDetails, getUse
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", authorize,getAllSubscriptions);
+
 subscriptionRouter.get("/:id", authorize,getSubscriptionDetails);
+
 subscriptionRouter.post("/", authorize,createSubscription);
+
+subscriptionRouter.get("/user/:id",authorize, getUserSubscriptions);
 
 subscriptionRouter.put("/", (req, res) => {
 res.send({ title: "Update subscription" }); 
 });
-subscriptionRouter.get("/user/:id",authorize, getUserSubscriptions);
-subscriptionRouter.put("/:id/cancel", (req, res) => {
-  res.send({ title: "GET all subscriptions" });
-});
-subscriptionRouter.put("/:id/cancel", (req, res) => {
-  res.send({ title: "CANCEL subscription" });
-});
-subscriptionRouter.get("/upcoming-renewals", (req, res) => {
-  res.send({ title: "GET upcoming renewals" });
-});
+
+
+// subscriptionRouter.put("/:id/cancel", (req, res) => {
+//   res.send({ title: "CANCEL subscription" });
+// });
+
+// subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+//   res.send({ title: "GET upcoming renewals" });
+// });
 
 export default subscriptionRouter
